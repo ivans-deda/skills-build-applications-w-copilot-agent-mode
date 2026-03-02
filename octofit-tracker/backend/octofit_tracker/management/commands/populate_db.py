b981b3afd4561511d06c8b05621a7842e97de0ac
ivans-deda <ivans.deda@gmail.com>
2026-03-02 04:24:05 +0000

    Implement initial Django project structure with models, views, serializers, and settings for octofit_tracker app

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,40 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        # Delete existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        users = [
+            User.objects.create(name='Spider-Man', email='spiderman@marvel.com', team=marvel),
+            User.objects.create(name='Iron Man', email='ironman@marvel.com', team=marvel),
+            User.objects.create(name='Wonder Woman', email='wonderwoman@dc.com', team=dc),
+            User.objects.create(name='Batman', email='batman@dc.com', team=dc),
+        ]
+
+        # Create activities
+        for user in users:
+            Activity.objects.create(user=user, type='Running', duration=30)
+            Activity.objects.create(user=user, type='Cycling', duration=45)
+
+        # Create workouts
+        Workout.objects.create(name='Hero HIIT', description='High intensity for heroes')
+        Workout.objects.create(name='Power Yoga', description='Yoga for super strength')
+
+        # Create leaderboard
+        Leaderboard.objects.create(team=marvel, points=100)
+        Leaderboard.objects.create(team=dc, points=90)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db populated with test data'))
